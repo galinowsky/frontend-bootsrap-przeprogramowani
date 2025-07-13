3786a6d6723a255196339ef82b8c2bfa842553be
galinowsky <thougless@gmail.com>
2025-07-13 17:44:49 +0200

    commit test

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ const App = () => {
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
         <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - Fan Service</h1>
+        <h2 className="text-lg text-center">Your one-stop solution for all things Rick and Morty!</h2>
       </div>
       <Outlet />
     </div>
